de8c53b21eb83b3ae58ad8f5f80c889f12227bf1
Jason Nam <contact@jasonnam.com>
2018-05-29 16:53:10 +0700

    Add template list command

---
+++ Sources/Flint/template/list/templateListCommandHandler.swift
@@ -0,0 +1,87 @@
+//
+//  templateListCommandHandler.swift
+//  Flint
+//
+//  Copyright (c) 2018 Jason Nam (https://jasonnam.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+import Bouncer
+import ANSIEscapeCode
+
+/// Template list command handler.
+let templateListCommandHandler: CommandHandler = { _, _, operandValues, optionValues in
+    // Grab values.
+    let verbose = optionValues.have(templateCloneVerboseOption)
+
+    // Print input summary.
+    if verbose {
+        printVerbose(
+            """
+            Input summary
+            └╴Verbose: \(verbose)
+            """
+        )
+    }
+
+    // List available templates.
+    do {
+        let templateDirectoryPath = try getTemplateDirectoryPath()
+        try templateDirectoryPath.enumerate { contentPath in
+            // Get template path.
+            let contentParentPath = contentPath.parent.path
+            let pathToPrint: String
+            if verbose {
+                pathToPrint = contentPath.parent.path
+            } else {
+                pathToPrint = String(contentParentPath.dropFirst(templateDirectoryPath.path.count + 1))
+            }
+
+            // Read template.
+            let template: Template
+            do {
+                if contentPath.name == jsonTemplateFileName {
+                    template = try readJSONTemplate(atPath: contentPath)
+                } else if contentPath.name == yamlTemplateFileName ||
+                    contentPath.name == ymlTemplateFileName {
+                    template = try readYAMLTemplate(atPath: contentPath)
+                } else {
+                    return
+                }
+            } catch {
+                print("\(pathToPrint.boldOutput) - \(error.localizedDescription.color(.red))")
+                return
+            }
+
+            // Print template info.
+            let output: String
+            if let description = template.description {
+                output = "\(pathToPrint.boldOutput) - \(description)"
+            } else {
+                output = pathToPrint.boldOutput
+            }
+            print(output)
+        }
+    } catch {
+        printError(error.localizedDescription)
+        return
+    }
+}
