b640a6143207ca4bf25b0cc1594002bb2d6926bb
jasonnam <contact@jasonnam.com>
2018-06-06 18:28:09 +0700

    Update sync version script

--- Scripts/SyncVersion.swift
+++ Scripts/SyncVersion.swift
@@ -8,6 +8,12 @@ let pathToVersionSwift = "Sources/Flint/version/version.swift"
 let version = try! String(contentsOfFile: pathToVersion, encoding: .utf8).dropLast()
 let versionSwift = try! String(contentsOfFile: pathToVersionSwift, encoding: .utf8)
 
-try! versionSwift
-    .replacingOccurrences(of: "develop", with: version)
-    .write(toFile: pathToVersionSwift, atomically: true, encoding: .utf8)
+var lines: [String] = []
+versionSwift.enumerateLines { line, _ in
+    lines.append(line)
+}
+lines.removeLast()
+lines.append("let version = \"\(version)\"")
+let newFileContent = lines.joined(separator: "\n")
+
+try! newFileContent.write(toFile: pathToVersionSwift, atomically: true, encoding: .utf8)
